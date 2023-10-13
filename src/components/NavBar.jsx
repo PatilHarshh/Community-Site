81036e81dd8268bceb4b156bbfffefb53c88d73d
Prashuu <97186762+prasad-chavan1@users.noreply.github.com>
2023-10-13 17:59:21 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
+import '../App.css';
 import { BrowserRouter as Router, NavLink } from "react-router-dom"; 
 
 export const NavBar = () => {
